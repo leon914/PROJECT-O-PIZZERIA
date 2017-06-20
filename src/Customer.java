4ae2eeb7f5fd95812adfdf043cabf68cd99256af
Your Name <you@example.com>
2017-06-20 09:55:51 +0100

    first upload

---
+++ src/Customer.java
@@ -0,0 +1,21 @@
+/**
+ * Created by lhi06 on 15/06/2017.
+ */
+public class Customer {
+
+    private String name;
+    private int age;
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
